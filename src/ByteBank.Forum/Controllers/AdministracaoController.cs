046047dc17b8b5fae8ec23433a9cfda82468c5bf
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-02-15 16:25:19 -0200

    Criação de controller de administração

---
+++ src/ByteBank.Forum/Controllers/AdministracaoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    [Authorize(Roles ="Administrador")]
+    public class AdministracaoController : Controller
+    {
+        public ActionResult Index()
+        {
+            System.Security.Claims.ClaimTypes.Name
+            return View();
+        }
+    }
+}
